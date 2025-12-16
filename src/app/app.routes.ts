1ba7bd0274828d70bf6cd7f028aef3586584a15d
lvlashnin <lvlashnin@gmail.com>
2025-12-16 18:12:52 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ResumeBuilder as ResumeBuilderComponent } from './features/resume-builder/resume-builder';
+import { Home as HomeComponent } from './features/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'builder', component: ResumeBuilderComponent },
+  { path: '**', redirectTo: '' },
+];
